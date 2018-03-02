65c5ffdb96e16e2aff3fd0d03c9c123667f2edfb
Mateusz_Thomas <mateuszthomas1@gmail.com>
2018-03-02 23:51:28 +0100

    [Android] added developer search by id number, simple menu and split the app into two activities

--- Android/ProjectsMap/app/src/main/java/project/projectsmap/MainActivity.java
+++ Android/ProjectsMap/app/src/main/java/project/projectsmap/MainActivity.java
@@ -1,6 +1,7 @@
 package project.projectsmap;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -15,84 +16,35 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button click;
-    public static TextView data;
-    ListView listView;
-    public static customAdapter custom;
+    Button clickAllDeveloper;
+    Button clickSerachDeveloper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        click = (Button) findViewById(R.id.button);
-//        data = (TextView) findViewById(R.id.fetcheddata);
+        clickAllDeveloper = (Button) findViewById(R.id.buttonAllDevelopers);
 
-        listView = (ListView)findViewById(R.id.listview);
-
-        custom = new customAdapter(this);
-
-        listView.setAdapter(custom);
-
-        click.setOnClickListener(new View.OnClickListener() {
+        clickAllDeveloper.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AllDevelopers.class);
+                startActivity(intent);
+            }
+        });
 
-                fetchData process = new fetchData();
-                process.execute();
+        clickSerachDeveloper = (Button) findViewById(R.id.buttonSearchDeveloper);
+
+        clickSerachDeveloper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SerachDeveloper.class);
+                startActivity(intent);
             }
         });
 
     }
+
 }
 
-class singleRow{
-    String RowData;
-
-    singleRow(String rowData){
-        this.RowData=rowData;
-    }
-}
-
-class customAdapter extends BaseAdapter{
-
-    ArrayList<singleRow> list;
-    Context c;
-
-    customAdapter(Context context){
-        c = context;
-        list = new ArrayList<singleRow>();
-        ////////////// minuta 16:00 link: https://www.youtube.com/watch?v=vpfeDoIWT0U !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    }
-
-    @Override
-    public int getCount() {
-        return list.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return list.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater layoutInflater = (LayoutInflater)c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-        View row = layoutInflater.inflate(R.layout.singlerow,parent,false);
-
-        TextView rowData = (TextView)row.findViewById(R.id.rowData);
-
-        singleRow tmp = list.get(position);
-
-        rowData.setText(tmp.RowData);
-
-
-        return row;
-    }
-}
