579c83fe5cdf747b518ee880c7f7aba24387998b
sebastian_bienert <226086@student.pwr.edu.pl>
2018-04-24 21:42:55 +0200

    [Backend] Azure Fixes part 4 - seed database

--- Backend/ProjectsMap.WebApi/ProjectsMap.WebApi/Migrations/Configuration.cs
+++ Backend/ProjectsMap.WebApi/ProjectsMap.WebApi/Migrations/Configuration.cs
@@ -1,3 +1,5 @@
+using ProjectsMap.WebApi.Repositories.EntityFramework;
+
 namespace ProjectsMap.WebApi.Migrations
 {
     using Microsoft.AspNet.Identity;
@@ -18,31 +20,8 @@ namespace ProjectsMap.WebApi.Migrations
 
         protected override void Seed(ProjectsMap.WebApi.Repositories.EfDbContext context)
         {
-            //var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new EfDbContext()));
-
-            //var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new EfDbContext()));
-
-            //var user = new ApplicationUser()
-            //{
-            //    UserName = "SuperPowerUser",
-            //    Email = "taiseer.joudeh@gmail.com",
-            //    EmailConfirmed = true,
-            //    JoinDate = DateTime.Now.AddYears(-3)
-            //};
-
-            //manager.Create(user, "MySuperP@ss!1");
-
-            //if (roleManager.Roles.Count() == 0)
-            //{
-            //    roleManager.Create(new IdentityRole { Name = "SuperAdmin" });
-            //    roleManager.Create(new IdentityRole { Name = "Admin" });
-            //    roleManager.Create(new IdentityRole { Name = "User" });
-            //}
-
-            //var adminUser = manager.FindByName("SuperPowerUser");
-
-            //manager.AddToRoles(adminUser.Id, new string[] { "SuperAdmin", "Admin" });
-            
+            var seed = new ProjectsMapDbInitializer();
+            seed.InitializeDatabase(context);
         }
     }
 }
